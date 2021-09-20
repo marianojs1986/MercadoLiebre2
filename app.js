9ed70dc278ea2e9f5b5eede9985785f5da29ba4f
marianojs86 <marianojs86@hotmail.com>
2021-09-19 23:11:18 -0300

    01 commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ("express");
+const path = require ("path");
+const app = express ();
+const port = process.env.PORT || 3000;
+
+app.listen (port, () => console.log ("server up at : http://localhost:3000/"))
+
+const publicPath = path.resolve (__dirname, "./public");
+app.use (express.static (publicPath))
+
+app.get ("/", (req, res) => {
+    res.sendFile(path.resolve (__dirname,"./views/home.html"))
+})
+
+app.get ("/login.html", (req, res) => {
+    res.sendFile(path.resolve (__dirname,"./views/login.html"))
+})
+
+app.get ("/register.html", (req, res) => {
+    res.sendFile(path.resolve (__dirname,"./views/register.html"))
+})
+    
+
